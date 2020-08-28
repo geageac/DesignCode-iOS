990107a699174601f2eda0e508279eed4d43b49a
Stanley <stanley.traub@gmail.com>
2020-08-27 22:01:34 -0700

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Stanley Traub on 8/27/20.
+//  Copyright © 2020 Stanley Traub. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
